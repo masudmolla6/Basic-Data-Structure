7c46ff25c8b632f182b2b5e4eceffbd200864d08
Masud Molla <masudmolla2937@gmail.com>
2025-08-25 18:39:44 +0600

    list element access and list literator

---
+++ Doubly LInked List/Module 10/list_iterators.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    list<int> myList={10, 20, 30, 40, 50};
+
+    // cout << *myList.begin() << endl;
+    cout << myList.back() << endl;
+    
+    return 0;
+}
