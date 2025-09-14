2acbe7e8cb2e030f9564839596170630c7341aa4
Masud Molla <masudmolla2937@gmail.com>
2025-09-15 00:44:11 +0600

    insert at tail

---
+++ Doubly LInked List/Conceptual Sessiom 01/insert_at_tail.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+        int val;
+        Node* pre;
+        Node* next;
+    Node(int val){
+        this->val=val;
+        this->pre=NULL;
+        this->next=NULL;
+    }
+};
+
+void insert_at_tail(Node* &head, Node* &tail, int val){
+    Node* newNode=new Node(val);
+    if(head==NULL){
+        head=newNode;
+        tail=newNode;
+    }
+    else{
+        tail->next=newNode;
+        newNode->pre=tail;
+        tail=newNode;
+    }
+}
+void print_forward(Node* temp){
+    if(temp==NULL){
+        return;
+    }
+
+    while (temp!=NULL)
+    {
+        cout << temp->val <<" ";
+        temp=temp->next;
+    }
+    
+}
+void print_backword(Node* temp){
+    if(temp==NULL){
+        return;
+    }
+    while(temp!=NULL){
+        cout << temp->val << " ";
+        temp=temp->pre;
+    }
+}
+int main()
+{
+    Node* head=NULL;
+    Node* tail=NULL;
+    int val;
+    while(cin>>val && val!=-1){
+        insert_at_tail(head, tail, val);
+    }
+    print_forward(head);
+    cout << endl;
+    print_backword(tail);
+    return 0;
+}
